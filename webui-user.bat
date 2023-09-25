205ddc6cf2a253cd4d9166492bee05c7039fdb23
KrimzenHart <englehat@oregonstate.edu>
2023-09-24 22:45:02 -0700

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,9 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\timot\AppData\Local\Programs\Python\Python311\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS= --no-half-vae --medvram 
+set SAFETENSORS_FAST_GPU=1
+git pull
 call webui.bat
